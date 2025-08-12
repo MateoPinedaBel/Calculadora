92206e0add130eaf033e65d2162f29e5b7065a6c
MateoPinedaBel <MateoPinedaBel@Outlook.com>
2025-08-12 11:16:36 -0500

    Feature: Creación de clase división.

---
+++ src/main/java/org/example/Division.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Division {
+    private int a;
+    private int b;
+
+    public Division(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public void dividir() {
+        try {
+            System.out.println(a / b);
+        } catch (ArithmeticException e) {
+            System.out.println("Error: No se puede dividir entre cero.");
+        }
+    }
+}
